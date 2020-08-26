c7e1b7db36681c58defc1f44ddaaa2b01a31f440
RM-Terrell <terrellsystems@gmail.com>
2020-08-25 19:05:53 -0600

    fixing timer

--- ToDoList/assets/js/todos.js
+++ ToDoList/assets/js/todos.js
@@ -1,9 +1,9 @@
-//! Toggle done/not done todos by clicking. 
+//! Toggle done/not done todos by clicking.
 
 //because this list starts with no "li" I have to first select the "ul" with an argument that when an "li" is created and clicked
 // INSIDE this "ul", run this code:
 $(".text").on("click", function () {
-  //create a separate class called 'completed' in css and toggle the class. 
+  //create a separate class called 'completed' in css and toggle the class.
   $(this).toggleClass("completed");
 });
 
@@ -12,8 +12,8 @@ $(".text").on("click", function () {
 $("#todoList").on("click", ".deleteHolder", function (event) {
   //remove the todo by clicking the X span, removes the li (parent element to the span)
   $(this).parent().fadeOut(500, function() {
-    // the second $(this) does not refer to the span, it is referring to the .parent() (li) element. I do this to 
-    // let the fadeout complete before the remove takes affect. 
+    // the second $(this) does not refer to the span, it is referring to the .parent() (li) element. I do this to
+    // let the fadeout complete before the remove takes affect.
     $(this).remove();
   });
   //prevent event bubbling: keeps the event from going up to parent elements. (wont go up to the li listener, then the ul, etc..)
@@ -30,7 +30,7 @@ $("input[type='text']").on("keypress", function(event){
     //empties input
     $(this).val("");
     //create a new li and add to ul
-    $("#todoList").append("<li><span class='deleteHolder'><i class='fas fa-trash-alt'></i> </span>" + todoText + 
+    $("#todoList").append("<li><span class='deleteHolder'><i class='fas fa-trash-alt'></i> </span>" + todoText +
       "<span class='buttonHolder'><button type='button' id='start'><i class='fas fa-play'></i></button></span></li></li>");
   }
 });
@@ -38,7 +38,7 @@ $("input[type='text']").on("keypress", function(event){
 function capitalize(inputString) {
   return inputString.charAt(0).toUpperCase() + inputString.slice(1).toLowerCase();
 };
- 
+
 //! Add/remove todo input
 
 $('.fa-minus').attr('title', 'Remove todo input').click(function() {
@@ -49,6 +49,7 @@ $('.fa-minus').attr('title', 'Remove todo input').click(function() {
 
 //! Timers
 
-$('#timer').click(function(){
-  $(this).stopwatch({format: '{M}m, {S.}'}).stopwatch('toggle');
+$('button.time-toggle-button').on('click', function() {
+  const element = $(this).closest('li.todo-list-item').find('.timer-output');
+  element.stopwatch({format: '{M}m, {S.}'}).stopwatch('toggle');
 });
